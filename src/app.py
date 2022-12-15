e2bd9edef4e4869291b95f3b3be240fa826e9dcd
Jules Delaire <ixirio76850@gmail.com>
2022-12-15 22:58:48 +0100

    added zombies template

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Hello World"
+	return render_template('socle/base.jinja')
+
+@app.route('/zombie')
+def zombie():
+	return render_template('zombies/zombies.jinja', zombie=1)
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
