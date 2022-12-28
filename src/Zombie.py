e26cf31054dcb4af5029b15c5ec7c847bcceed68
Jules Delaire <ixirio76850@gmail.com>
2022-12-28 23:32:26 +0100

    added zombie class + manage css zombie placement

---
+++ src/Zombie.py
@@ -0,0 +1,35 @@
+import random
+
+class Zombie:
+
+    body : dict
+    head : dict
+    leftArm : dict
+    rightArm : dict
+    leftLeg : dict
+    rightLeg : dict
+
+    def __init__(self) -> None:
+        self.body = self.getZombiePart('body', True)
+        self.head = self.getZombiePart('head')
+        self.leftArm = self.getZombiePart('left_arm')
+        self.rightArm = self.getZombiePart('right_arm')
+        self.leftLeg = self.getZombiePart('left_leg')
+        self.rightLeg = self.getZombiePart('right_leg')
+
+    def __init__(self, fullBody: bool) -> None:
+        self.body = self.getZombiePart('body', True)
+        self.head = self.getZombiePart('head', fullBody)
+        self.leftArm = self.getZombiePart('left_arm', fullBody)
+        self.rightArm = self.getZombiePart('right_arm', fullBody)
+        self.leftLeg = self.getZombiePart('left_leg', fullBody)
+        self.rightLeg = self.getZombiePart('right_leg', fullBody)
+
+    def getRandomBool(self) -> bool:
+        return bool(random.getrandbits(1))
+
+    def getZombiePart(self, element: str, required: bool = False) -> dict:
+        return {
+            'has' : True if required else self.getRandomBool(),
+            'path': f'/static/zombie/{element}.png'
+        }
--- src/app.py
+++ src/app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from Zombie import Zombie
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return render_template('socle/base.jinja')
+	return render_template('zombies/zombies.jinja', zombie=Zombie(True))
 
 @app.route('/zombie')
 def zombie():
-	return render_template('zombies/zombies.jinja', zombie=1)
+
+	fullBody: bool = request.args.get('fullBody')
+
+	return render_template('zombies/zombies.jinja', zombie=Zombie(fullBody))
+
+@app.route('/zombies/<n>')
+def zombies(n):
+
+	fullBody: bool = request.args.get('fullBody')
+
+	zombies: list[Zombie] = [ Zombie(fullBody) for _ in range(int(n)) ]
+
+	return render_template('zombies/zombies.jinja', zombies=zombies)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return redirect(url_for('zombie'))
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
